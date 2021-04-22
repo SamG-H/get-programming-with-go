3fa93d8aae9d83e50535518e08253e3debd5a5c0
Samuel Grasse-Haroldsen <sgrasseharoldsen@gmail.com>
2021-04-21 18:53:41 -0700

    refactor

--- unit3/lesson15/tempCapstone.go
+++ unit3/lesson15/tempCapstone.go
@@ -2,12 +2,15 @@ package main
 
 import "fmt"
 
-type celsius float64
-type fahrenheit float64
-type kelvin float64
-type getDataFunc func(int) (string, string, string, int, int, int)
-
 const tempFmt = "%.1f"
+
+const (
+	PrintColor = "\033[38;5;%dm%-9v\033[39;49m"
+)
+
+type getDataFunc func(int) (col1, col2, col3 string, col1color, col2color, col3color int)
+
+type kelvin float64
 
 func (k kelvin) fahrenheit() fahrenheit {
 	return k.celsius().fahrenheit()
@@ -24,9 +27,11 @@ func (k kelvin) color() int {
 	case k >= 373.1:
 		return 1
 	default:
-		return 7
+		return 2
 	}
 }
+
+type fahrenheit float64
 
 func (f fahrenheit) celsius() celsius {
 	return celsius((f - 32.0) * (5.0 / 9.0))
@@ -43,9 +48,11 @@ func (f fahrenheit) color() int {
 	case f >= 212:
 		return 1
 	default:
-		return 7
+		return 2
 	}
 }
+
+type celsius float64
 
 func (c celsius) fahrenheit() fahrenheit {
 	return fahrenheit(c*(9.0/5.0) + 32.0)
@@ -62,13 +69,9 @@ func (c celsius) color() int {
 	case c >= 100:
 		return 1
 	default:
-		return 7
+		return 2
 	}
 }
-
-const (
-	PrintColor = "\033[38;5;%dm%-9v\033[39;49m"
-)
 
 func drawHeader(title1, title2, title3 string) {
 	drawRowSeparator()
@@ -87,19 +90,24 @@ func drawRow(column1, column2, column3 string, color1, color2, color3 int) {
 	fmt.Printf("|\n")
 }
 
-func fToCToK(rowNum int) (string, string, string, int, int, int) {
+func fToCToK(rowNum int) (col1, col2, col3 string, fColor, cColor, kColor int) {
 	f := fahrenheit((rowNum * 5) - 40)
 	c := f.celsius()
 	k := f.kelvin()
-	col1 := fmt.Sprintf(tempFmt, f)
-	col2 := fmt.Sprintf(tempFmt, c)
-	col3 := fmt.Sprintf(tempFmt, k)
-	return col1, col2, col3, f.color(), c.color(), k.color()
+	col1 = fmt.Sprintf(tempFmt, f)
+	col2 = fmt.Sprintf(tempFmt, c)
+	col3 = fmt.Sprintf(tempFmt, k)
+	fColor = f.color()
+	cColor = c.color()
+	kColor = k.color()
+	return col1, col2, col3, fColor, cColor, kColor
 }
 
 func cToFToK(rowNum int) (col1, col2, col3 string, cColor, fColor, kColor int) {
-	// when declaring return types with a name it is like you are
+	// when declaring return types with names it is like you are
 	// initializing them
+	// if you name return types you can do a naked return
+	// and just type in return (like implicit return in Ruby)
 	c := celsius((rowNum * 5) - 40)
 	f := c.fahrenheit()
 	k := c.kelvin()
@@ -112,14 +120,19 @@ func cToFToK(rowNum int) (col1, col2, col3 string, cColor, fColor, kColor int) {
 	return col1, col2, col3, cColor, fColor, kColor
 }
 
-func kToFToC(rowNum int) (string, string, string, int, int, int) {
+// add names to multi returns & single return if value isn't obvious
+// function names should convey meaning
+func kToFToC(rowNum int) (col1, col2, col3 string, kColor, fColor, cColor int) {
 	k := kelvin(rowNum * 5)
 	f := k.fahrenheit()
 	c := k.celsius()
-	col1 := fmt.Sprintf(tempFmt, k)
-	col2 := fmt.Sprintf(tempFmt, f)
-	col3 := fmt.Sprintf(tempFmt, c)
-	return col1, col2, col3, k.color(), f.color(), c.color()
+	col1 = fmt.Sprintf(tempFmt, k)
+	col2 = fmt.Sprintf(tempFmt, f)
+	col3 = fmt.Sprintf(tempFmt, c)
+	kColor = k.color()
+	fColor = f.color()
+	cColor = c.color()
+	return col1, col2, col3, kColor, fColor, cColor
 }
 
 func drawTable(hdr1, hdr2, hdr3 string, numRows int, getData getDataFunc) {
@@ -132,7 +145,7 @@ func drawTable(hdr1, hdr2, hdr3 string, numRows int, getData getDataFunc) {
 }
 
 func main() {
-	drawTable("°F", "°C", "°K", 66, fToCToK)
-	drawTable("°C", "°F", "°K", 37, cToFToK)
-	drawTable("°K", "°F", "°C", 130, kToFToC)
+	drawTable("°F", "°C", "K", 66, fToCToK)
+	drawTable("°C", "°F", "K", 37, cToFToK)
+	drawTable("K", "°F", "°C", 130, kToFToC)
 }
